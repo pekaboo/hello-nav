42d3d83a9e27f625b06f91a51863481d62050870
学长的猫 <me.wangjun@qq.com>
2024-09-05 17:40:50 +0800

    add 接口调整了 从 NotionTable

--- src/model/index.ts
+++ src/model/index.ts
@@ -5,8 +5,64 @@ async function getAppItems(url: string): Promise<AppItem[]> {
 }
 async function getModules(context: Record<string, AppItem[]>): Promise<CateItem[]> {
   if (import.meta.env.VITE_USE_MENU_URL) { 
-    const result: any = await getAppItems(import.meta.env.VITE_USE_MENU_URL)
-    return result
+    let result: any = await getAppItems(import.meta.env.VITE_USE_MENU_URL)
+    console.log(result)
+    let rows = result.data.rows
+    let groups: CateItem[] = []
+    rows.forEach((item: { cat: string; id: string; url: string; name: string; code: string ;icon:string}) => {
+      let group = groups.find(g => g.title === item.cat)
+      if (!group) {
+        group = { title: item.cat, children: [] }
+        groups.push(group)
+      }
+      group.children.push({ 
+        homepage: item.url,
+        repository: item.url,
+        name: item.name,
+        "icon": item.icon||'',
+        "keywords": [
+          "code",
+          item.cat
+         ],
+      })
+    })
+    console.log(groups)
+//     DEMO
+//     [
+//     {
+//         "title": "自留地",
+//         "children": [
+//             {
+//                 "homepage": "https://poe.com",
+//                 "repository": "https://poe.com",
+//                 "icon": "",
+//                 "keywords": [
+//                     "poe",
+//                     "poe"
+//                 ],
+//                 "name": "POE"
+//             },
+//             {
+//                 "homepage": "https://devv.ai/",
+//                 "name": "devv.ai"
+//             },
+//             {
+//                 "homepage": "https://search.glarity.ai/",
+//                 "name": "glarity.ai"
+//             }
+//         ]
+//     },
+//     {
+//         "title": "每天",
+//         "children": [
+//             {
+//                 "homepage": "https://www.x.com/",
+//                 "name": "twiter"
+//             }
+//         ]
+//     }
+// ]
+    return groups
   }
   const result: CateItem[] = [];
   for (const path in context) {
