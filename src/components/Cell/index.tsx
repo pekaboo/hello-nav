e4f87af55e8f5a7e0c76e1d2c7b42a0f8970c96e
p-wangjun106 <p-wangjun106@xiaomi.com>
2025-02-27 17:19:38 +0800

    fix icon

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -7,16 +7,16 @@ function onCornerClick(e: React.SyntheticEvent, repository: string) {
   return false
 }
 
-function getImgSrc(fileName: string,home: string): string {
+function getImgSrc(fileName: string,home: string,repository: string): string {
   function extractHostname(url: string) {
     const parsedUrl = new URL(url);
-    return parsedUrl.hostname;
+    return parsedUrl.hostname.replace('www.', '');
   }
   // 判断 fileName 是否是一个 URL
   // 如果是 URL 则直接返回 fileName
   if (/^https?:\/\//.test(fileName)) return fileName
   //如果为空。根据接口返回
-  if (!fileName) return  "https://api.iowen.cn/favicon/" + extractHostname(home)+'.png';
+  if (!fileName) return  "https://logo.clearbit.com/"   + extractHostname(repository);
   return new URL(`../../assets/icons/${fileName}`, import.meta.url).href
 }
 
@@ -36,7 +36,7 @@ const Cell = ({ homepage, icon, repository, name, darkInvert, lessRadius }: AppI
     <li className="cell">
       <a className="app" href={homepage} title={name}>
         <div className="img-box">
-          <img src={getImgSrc(icon,homepage)} className={imgClass} alt={name} />
+          <img onError={(e) => { e.currentTarget.src = new URL(`../../assets/icons/default-icon.svg`, import.meta.url).href; }} src={getImgSrc(icon,homepage,repository||'')} className={imgClass} alt={name} />
         </div>
         <p className="title" data-size={size}>
           {name}
